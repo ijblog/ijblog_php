bd3f1beda946f8b393e40f3777b05fcec12561fb
假如可以选择 <ijblog>
2019-03-28 14:42:36 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

--- demo.py
+++ demo.py
@@ -12,3 +12,13 @@ total = 3 + 4 \
 print (total)
 print (3E-2)
 print (1 + 2j)
+print (r"this is a line with \n")
+print ("this is a line with \n")
+boss = "Ashake艾萨克"
+print (boss)
+print (boss[0:-1])
+print (boss[0])
+print (boss[0:])
+print (boss[2:7])
+print (boss * 2)
+print (boss + 'boos')

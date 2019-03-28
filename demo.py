042b00616f3e288a295dfa9d6e4245efe18c7e9b
假如可以选择 <ijblog>
2019-03-28 13:44:23 +0800

    由 Cloud Studio 更新

--- demo.py
+++ demo.py
@@ -2,3 +2,13 @@
  
 # 第一个注释
 print ("Hello, Python!") # 第二个注释
+if True:
+    print ("true")
+else:
+    print ("fale")
+total = 3 + 4 \
+        + 4 + \
+        5 + 2
+print (total)
+print (3E-2)
+print (1 + 2j)

798ea6227f5d434992677df6a73f2df0582cf7d4
假如可以选择 <ijblog>
2019-03-28 16:37:23 +0800

    python 3 +++

--- demo.py
+++ demo.py
@@ -2,24 +2,20 @@
  
 # 第一个注释
 print ("Hello, Python!") # 第二个注释
-if True:
-    print ("true")
-else:
-    print ("fale")
-total = 3 + 4 \
-        + 4 + \
-        5 + 2
-print (total)
-print (3E-2)
+print ()
+print (3E-2 , end = " ")
 print (1 + 2j)
-print (r"this is a line with \n")
-print ("this is a line with \n")
-boss = "Ashake艾萨克"
-print (boss)
-print (boss[0:-1])
-print (boss[0])
-print (boss[0:])
-print (boss[2:7])
-print (boss * 2)
-print (boss + 'boos')
-print (r'boos\naske')
+input ("\n\n按下 enter 键后退出。")
+print ()
+"""
+import sys
+print ('================Python import mode==========================')
+print ('命令行参数为:')
+for i in sys.argv:
+    print (i)
+print ('\n python 路径为' , sys.path)
+"""
+
+from sys import argv,path
+
+print ('path:', path)

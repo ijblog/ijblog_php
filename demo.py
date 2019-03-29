8d651d1c75e6301d7433be9a936c42a7392026c0
假如可以选择 <ijblog>
2019-03-29 18:15:11 +0800

    由 Cloud Studio 更新，https://studio.dev.tencent.com

--- demo.py
+++ demo.py
@@ -23,4 +23,5 @@ print ('path:', path)
 a, b, c, d = 20, 5.5, True, 4+3j
 print(type(a), type(b), type(c), type(d))
 a = 111
-print( isinstance(a, int) )
+print ( isinstance(a, int) )
+print (2 ** 5)

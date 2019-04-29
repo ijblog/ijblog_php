cca418d118aed0c526b0757993fd24f3a1b45455
假如可以选择 <ijblog>
2019-04-29 17:51:18 +0800

    嘿嘿嘿

--- demo.py
+++ demo.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
  
-# 第一个注释
-print ("Hello, World!") # 第二个注释
+tup1 = (12, 34.56);
+tup2 = ('abc', 'xyz')
+ 
+# 以下修改元组元素操作是非法的。
+# tup1[0] = 100
+ 
+# 创建一个新的元组
+tup3 = tup1 + tup2;
+print (tup3)
